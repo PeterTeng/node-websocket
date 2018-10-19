2aedb60b74599b4357eddd5c36c26b0881773362
DONGSHIH TENG <tou9702@gmail.com>
2018-10-19 22:39:13 +0900

    Send message from client

--- server/main.js
+++ server/main.js
@@ -6,6 +6,9 @@ var server = new Server({ port: 9000 });
 server.on("connection", function(webSocket) {
   webSocket.on("message", function(message) {
     console.log(`Message Received: ${message}`);
+    server.clients.forEach(function(client){
+      client.send(`${new Date()}: ${message}`);
+    });
   });
 
   webSocket.on("close", function() {
