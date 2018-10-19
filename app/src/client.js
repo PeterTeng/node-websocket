d8fc36b056374e6dc28bf448ec2abaee5c3641cf
DONGSHIH TENG <tou9702@gmail.com>
2018-10-19 21:39:59 +0900

    Send clicked message

--- app/src/client.js
+++ app/src/client.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
   webSocket.addEventListener("open", function(event) {
     window.notify_bar("Connected to Socket Server.");
   });
+
+  webSocket.addEventListener("message", function(event) {
+    console.log('Message from server ', event.data);
+  });
+
+  $('.send-msg-btn').on("click", function() {
+    webSocket.send("Clicked!");
+  })
 });
